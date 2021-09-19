9a1cf7a2f6d1ffc7303930d47dffa578b5c4e8e5
sharp_NEKO <65962822+sharp-NEKO@users.noreply.github.com>
2021-09-20 05:05:40 +0900

    翻訳と修正

---
+++ kounter/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/sharp/Documents/Qt_Learn/kounter
+# Build directory: /home/sharp/Documents/Qt_Learn/kounter/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(appstreamtest "/app/bin/cmake" "-DAPPSTREAMCLI=/usr/bin/appstreamcli" "-DINSTALL_FILES=/home/sharp/Documents/Qt_Learn/kounter/build/install_manifest.txt" "-P" "/usr/share/ECM/kde-modules/appstreamtest.cmake")
+set_tests_properties(appstreamtest PROPERTIES  _BACKTRACE_TRIPLES "/usr/share/ECM/kde-modules/KDECMakeSettings.cmake;161;add_test;/usr/share/ECM/kde-modules/KDECMakeSettings.cmake;179;appstreamtest;/usr/share/ECM/kde-modules/KDECMakeSettings.cmake;0;;/home/sharp/Documents/Qt_Learn/kounter/CMakeLists.txt;12;include;/home/sharp/Documents/Qt_Learn/kounter/CMakeLists.txt;0;")
+subdirs("src")
---
+++ kounter/build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sharp/Documents/Qt_Learn/kounter/src
+# Build directory: /home/sharp/Documents/Qt_Learn/kounter/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
